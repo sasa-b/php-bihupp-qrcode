08c2bf83442ef2ba6ec08f4f2209811e7021f28f
Sasa Blagojevic <sasa.blagojevic@mail.com>
2026-04-14 21:25:52 +0200

    add doc blocks to read sources

--- src/QRCode/ReadSource/Blob.php
+++ src/QRCode/ReadSource/Blob.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Sco\BihuppQRCode\QRCode\ReadSource;
 
+use Sco\BihuppQRCode\QRCode\ImageExtension;
 use Sco\BihuppQRCode\QRCode\ReadSource;
 
 final readonly class Blob extends ReadSource
 {
+    /**
+     * @param string         $value     Raw binary blob of the QR code image
+     * @param ImageExtension $extension Image processing extension to use
+     */
+    public function __construct(string $value, ImageExtension $extension = ImageExtension::GD)
+    {
+        parent::__construct($value, $extension);
+    }
 }
